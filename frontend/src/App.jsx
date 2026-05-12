cfd796c14e2c110c7317c2dbcc65ccb6d66f832f
Kuldeep-singh24 <143269323+Kuldeep-singh24@users.noreply.github.com>
2026-05-12 13:37:05 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import HomePage from './components/HomePage'
-import AboutUs from './components/AboutUs'
 import Datasets from './components/Datasets'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 
 const router  = createBrowserRouter([
   { path: '/', element: <HomePage /> },
-  {path: '/about', element: <AboutUs /> },
   {path:'/datasets', element: <Datasets/>},
 ])
 
